f18b826a952be5b916ff97766d2ac0a5bf967b5c
[Bios]Marcel <marceloschr@googlemail.com>
2018-03-24 12:01:49 +0100

    # Updated Kotlin and Spring
    * Removed Spring Boot mainClass declaration (bootRun Task now works)

--- src/main/kotlin/com/msc/mastermind/Application.kt
+++ src/main/kotlin/com/msc/mastermind/Application.kt
@@ -2,8 +2,10 @@ package com.msc.mastermind
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 
+@SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
 class Application
